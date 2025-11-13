accb5f395cceb6539f8a2cf123548fd7ec7a1607
vanquan0408 <lovehiiro1@gmail.com>
2025-11-13 20:41:19 +0700

    quân up

--- src/Controllers/TrangChuController.php
+++ src/Controllers/TrangChuController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Website\TinTuc\Controllers;
 
 use Website\TinTuc\Models\ChuyenMucModel;
@@ -6,8 +7,10 @@ use Website\TinTuc\Models\BaiVietModel;
 use Website\TinTuc\Models\QuangCaoModel;
 use Website\TinTuc\Models\BannerModel; // thêm dòng này nếu bạn có BannerModel
 
-class TrangChuController {
-    public function index() {
+class TrangChuController
+{
+    public function index()
+    {
         $baiVietModel = new BaiVietModel();
         // --- Lấy danh mục chuyên mục ---
         $chuyenMucModel = new ChuyenMucModel();
@@ -26,10 +29,14 @@ class TrangChuController {
         $tinMoiNhat = $baiVietModel->getTinMoiNhat(6);
         $tinNoiBat = $baiVietModel->getTinNoiBat(5);
         $tinXemNhieu = $baiVietModel->getTinXemNhieu(5);
-
+        // --- Lấy bài viết theo từng chuyên mục (phục vụ phần “📂 Bài viết theo chuyên mục”) ---
+        $baiVietTheoChuyenMuc = [];
+        foreach ($chuyenMuc as $cm) {
+            // Lấy toàn bộ bài viết theo chuyên mục, từ mới đến cũ
+            $baiVietTheoChuyenMuc[$cm['id']] = $baiVietModel->getTinTheoChuyenMuc($cm['id']);
+        }
 
         // Biến $banners, $tinMoiNhat... sẽ có sẵn trong view
         include __DIR__ . '/../../views/frontend/trang_chu.php';
     }
-    
 }
