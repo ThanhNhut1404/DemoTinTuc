6c0e6f0c9ad90d5b8a714b3ca4c40899ac7dc0ed
phong204376 <thanhphong7562@gmail.com>
2025-11-13 19:09:12 +0700

    phong đẩy lên nè

---
+++ partials/footer.php
@@ -0,0 +1,6 @@
+<!-- views/frontend/_footer.php -->
+<footer class="bg-light text-center py-4 mt-5 border-top">
+    <small>© <?= date('Y') ?> QuanNhiPhongNghiNhutPhi.</small>
+</footer>
+</body>
+</html>
