70a4d1be426692b2a09cc22d367615f8c9690291
ThanhNhut1404 <huynhthanhnhut144@gmail.com>
2025-11-28 10:58:11 +0700

    note lại

--- src/Models/ThanhVienModel.php
+++ src/Models/ThanhVienModel.php
@@ -160,7 +160,6 @@ class ThanhVienModel {
     }
 
     // Tìm kiếm người dùng theo tên hoặc email
-    // Tìm kiếm người dùng theo tên hoặc email
     public function search(string $keyword, ?string $role = null, ?string $status = null, ?string $gender = null) {
         $idCol = $this->cols['id'];
         $nameCol = $this->cols['ho_ten'];
